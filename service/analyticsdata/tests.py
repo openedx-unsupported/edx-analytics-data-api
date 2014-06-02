2afa2698713e4ee9494145d70334f8474474f5a4
Gabe Mulley <gabe@edx.org>
2014-06-02 16:18:19 -0400

    Initialize django project
    Change-Id: Idd7e1a8e370e6fe216ec7413c26de66b8d51ff38

---
+++ service/analyticsdata/tests.py
@@ -0,0 +1,84 @@
+
+from django.contrib.auth.models import User
+from django.db.utils import ConnectionHandler
+from django.test import TestCase
+from mock import patch
+from rest_framework.authtoken.models import Token
+
+from analyticsdata.views import handle_internal_server_error, handle_missing_resource_error
+
+# NOTE: Full URLs are used throughout these tests to ensure that the API contract is fulfilled. The URLs should *not*
+# change for versions greater than 1.0.0. Tests target a specific version of the API, additional tests should be added
+# for subsequent versions if there are breaking changes introduced in those versions.
+
+
+class OperationalEndpointsTest(TestCase):
+
+    def test_status(self):
+        response = self.client.get('/api/v0/status')
+        self.assertEquals(response.status_code, 200)
+
+    def test_authentication_check_failure(self):
+        response = self.client.get('/api/v0/authenticated')
+        self.assertEquals(response.status_code, 401)
+
+    def test_authentication_check_success(self):
+        test_user = User.objects.create_user('tester', 'test@example.com', 'testpassword')
+        token = Token.objects.create(user=test_user)
+        response = self.client.get('/api/v0/authenticated', HTTP_AUTHORIZATION='Token ' + token.key)
+        self.assertEquals(response.status_code, 200)
+
+    def test_health(self):
+        response = self.client.get('/api/v0/health')
+        self.assertEquals(
+            response.data,
+            {
+                'overall_status': 'OK',
+                'detailed_status': {
+                    'database_connections': {
+                        'default': 'OK'
+                    }
+                }
+            }
+        )
+        self.assertEquals(response.status_code, 200)
+
+    def test_database_down(self):
+        databases = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:"
+            },
+            "reporting": {}
+        }
+        with patch('analyticsdata.views.connections', ConnectionHandler(databases)):
+            response = self.client.get('/api/v0/health')
+            self.assertEquals(
+                response.data,
+                {
+                    'overall_status': 'UNAVAILABLE',
+                    'detailed_status': {
+                        'database_connections': {
+                            'default': 'OK',
+                            'reporting': 'UNAVAILABLE'
+                        }
+                    }
+                }
+            )
+            self.assertEquals(response.status_code, 200)
+
+
+class ErrorHandlingTest(TestCase):
+
+    def test_internal_server_error_handling(self):
+        response = handle_internal_server_error(None)
+        self.validate_error_response(response, 500)
+
+    def validate_error_response(self, response, status_code):
+        self.assertEquals(response.content, '{{"status": {0}}}'.format(status_code))
+        self.assertEquals(response.status_code, status_code)
+        self.assertEquals(response.get('Content-Type'), 'application/json; charset=utf-8')
+
+    def test_missing_resource_handling(self):
+        response = handle_missing_resource_error(None)
+        self.validate_error_response(response, 404)

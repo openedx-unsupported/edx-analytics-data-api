6ff0a16e4b9f6b0e1aabfa50135f59ec6e36042c
John Jarvis <john@jarv.org>
2014-06-25 16:01:05 -0400

    fix os.environ

--- analyticsdataserver/settings/production.py
+++ analyticsdataserver/settings/production.py
@@ -34,12 +34,12 @@ with open(CONFIG_FILE) as f:
 vars().update(config_from_yaml)
 
 DB_OVERRIDES = dict(
-    PASSWORD=os.environ.get('DB_MIGRATION_PASS', DATABASES['default']['PASS']),
-    ENGINE=os.environ.get('DB_MIGRATION_ENGINE', DATABASES['default']['ENGINE']),
-    USER=os.environ.get('DB_MIGRATION_USER', DATABASES['default']['USER']),
-    NAME=os.environ.get('DB_MIGRATION_NAME', DATABASES['default']['NAME']),
-    HOST=os.environ.get('DB_MIGRATION_HOST', DATABASES['default']['HOST']),
-    PORT=os.environ.get('DB_MIGRATION_PORT', DATABASES['default']['PORT']),
+    PASSWORD=environ.get('DB_MIGRATION_PASS', DATABASES['default']['PASS']),
+    ENGINE=environ.get('DB_MIGRATION_ENGINE', DATABASES['default']['ENGINE']),
+    USER=environ.get('DB_MIGRATION_USER', DATABASES['default']['USER']),
+    NAME=environ.get('DB_MIGRATION_NAME', DATABASES['default']['NAME']),
+    HOST=environ.get('DB_MIGRATION_HOST', DATABASES['default']['HOST']),
+    PORT=environ.get('DB_MIGRATION_PORT', DATABASES['default']['PORT']),
 )
 
 for override, value in DB_OVERRIDES.iteritems():

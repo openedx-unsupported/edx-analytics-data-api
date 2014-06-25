6fa47585b6fb21d594c9dac3bb7627464e070a14
John Jarvis <john@jarv.org>
2014-06-25 15:56:14 -0400

    adding overrides from env

--- analyticsdataserver/settings/production.py
+++ analyticsdataserver/settings/production.py
@@ -32,3 +32,15 @@ with open(CONFIG_FILE) as f:
   config_from_yaml = yaml.load(f)
 
 vars().update(config_from_yaml)
+
+DB_OVERRIDES = dict(
+    PASSWORD=os.environ.get('DB_MIGRATION_PASS', DATABASES['default']['PASS']),
+    ENGINE=os.environ.get('DB_MIGRATION_ENGINE', DATABASES['default']['ENGINE']),
+    USER=os.environ.get('DB_MIGRATION_USER', DATABASES['default']['USER']),
+    NAME=os.environ.get('DB_MIGRATION_NAME', DATABASES['default']['NAME']),
+    HOST=os.environ.get('DB_MIGRATION_HOST', DATABASES['default']['HOST']),
+    PORT=os.environ.get('DB_MIGRATION_PORT', DATABASES['default']['PORT']),
+)
+
+for override, value in DB_OVERRIDES.iteritems():
+    DATABASES['default'][override] = value

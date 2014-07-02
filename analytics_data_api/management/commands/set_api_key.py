c7e6195ef544d60499cedbf2963866722247bf23
Clinton Blackburn <cblackburn@edx.org>
2014-07-02 13:03:14 -0400

    Added Course Enrollment-Demographic Resources
    Change-Id: I5285f72b5bffb59a97cdc4750e90cd8578202b83

---
+++ analytics_data_api/management/commands/set_api_key.py
@@ -0,0 +1,39 @@
+"""A command to set the API key for a user using when using TokenAuthentication."""
+
+from optparse import make_option
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from analytics_data_api.utils import delete_user_auth_token, set_user_auth_token
+
+
+class Command(BaseCommand):
+    """A command to set the API key for a user using when using TokenAuthentication."""
+
+    help = 'Set the API key for the specified user.'
+    args = '<username> <api_key>'
+    option_list = BaseCommand.option_list + (
+        make_option('--delete-key', action='store_true', default=False, help="Delete API key for user"),
+    )
+
+    def handle(self, *args, **options):
+        if len(args) < 1:
+            raise CommandError("You must supply a username.")
+
+        username = args[0]
+
+        if options['delete_key']:
+            delete_user_auth_token(username)
+            print 'Removed API key for user: <{0}>'.format(username)
+        else:
+            if len(args) < 2:
+                raise CommandError("You must supply both a username and key.")
+
+            user, _ = User.objects.get_or_create(username=username)
+
+            try:
+                key = args[1]
+                set_user_auth_token(user, key)
+            except AttributeError:
+                print "The key %s is in use by another user. Please select another key." % key
--- analyticsdataserver/urls.py
+++ analyticsdataserver/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 urlpatterns = patterns(
     '',
+    url(r'^$', RedirectView.as_view(url='/docs')),
+
     # Support logging in to the browseable API
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
@@ -12,15 +15,13 @@ urlpatterns = patterns(
     url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
 
     # Route all reports URLs to this endpoint
-    # TODO: come up with a better way to handle versioning, maybe a decorator?
-    url(r'^api/v0/', include('analyticsdata.urls')),
+    url(r'^api/', include('analytics_data_api.urls', namespace='api')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 )
-
 
 if settings.ENABLE_ADMIN_SITE:
     admin.autodiscover()
     urlpatterns += patterns('', url(r'^site/admin/', include(admin.site.urls)))
 
-
-handler500 = 'analyticsdata.views.handle_internal_server_error'  # pylint: disable=invalid-name
-handler404 = 'analyticsdata.views.handle_missing_resource_error'  # pylint: disable=invalid-name
+handler500 = 'analyticsdataserver.views.handle_internal_server_error'  # pylint: disable=invalid-name
+handler404 = 'analyticsdataserver.views.handle_missing_resource_error'  # pylint: disable=invalid-name

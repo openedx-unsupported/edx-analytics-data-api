b43ddb4160cd3d95ee938bcfdc5942ad9422cd1a
Dennis Jen <djen@edx.org>
2016-12-28 17:40:08 -0500

    Upgraded django, drf, and removed django-model-utils.

--- analyticsdataserver/router.py
+++ analyticsdataserver/router.py
@@ -3,22 +3,25 @@ from django.conf import settings
 
 class AnalyticsApiRouter(object):
     def db_for_read(self, model, **hints):  # pylint: disable=unused-argument
-        return self._get_database(model)
+        # pylint: disable=protected-access
+        return self._get_database(model._meta.app_label)
 
-    def _get_database(self, model):
-        if model._meta.app_label == 'v0':   # pylint: disable=protected-access
+    def _get_database(self, app_label):
+        if app_label == 'v0':
             return getattr(settings, 'ANALYTICS_DATABASE', 'default')
 
         return None
 
     def db_for_write(self, model, **hints):  # pylint: disable=unused-argument
-        return self._get_database(model)
+        # pylint: disable=protected-access
+        return self._get_database(model._meta.app_label)
 
     def allow_relation(self, obj1, obj2, **hints):  # pylint: disable=unused-argument
-        return self._get_database(obj1) == self._get_database(obj2)
+        # pylint: disable=protected-access
+        return self._get_database(obj1._meta.app_label) == self._get_database(obj2._meta.app_label)
 
-    def allow_migrate(self, database, model):
-        dest_db = self._get_database(model)
+    def allow_migrate(self, database, app_label, model_name=None, **hints):  # pylint: disable=unused-argument
+        dest_db = self._get_database(app_label)
         if dest_db is not None:
             return database == dest_db
         else:

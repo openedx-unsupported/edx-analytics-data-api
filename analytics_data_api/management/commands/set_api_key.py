d9a771dac9d203238fc17c0dcad457bb99f2b727
Dennis Jen <dsjen@cs.unc.edu>
2017-01-03 14:51:42 -0500

    Upgraded django, drf, and removed django-model-utils. (#149)

--- analytics_data_api/management/commands/set_api_key.py
+++ analytics_data_api/management/commands/set_api_key.py
@@ -1,7 +1,5 @@
 """A command to set the API key for a user using when using TokenAuthentication."""
 
-from optparse import make_option
-
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 
@@ -15,29 +13,35 @@ class Command(BaseCommand):
     """A command to set the API key for a user using when using TokenAuthentication."""
 
     help = 'Set the API key for the specified user.'
-    args = '<username> <api_key>'
-    option_list = BaseCommand.option_list + (
-        make_option('--delete-key', action='store_true', default=False, help="Delete API key for user"),
-    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs='?')
+        parser.add_argument('api_key', nargs='?')
+        parser.add_argument(
+            '--delete-key',
+            action='store_true',
+            default=False,
+            help="Delete API key for user",
+        )
 
     def handle(self, *args, **options):
-        if len(args) < 1:
+        if options['username'] is None:
             raise CommandError("You must supply a username.")
 
-        username = args[0]
+        username = options['username']
 
         if options['delete_key']:
             delete_user_auth_token(username)
             print 'Removed API key for user: <{0}>'.format(username)
         else:
-            if len(args) < 2:
+            if options['api_key'] is None:
                 raise CommandError("You must supply both a username and key.")
 
             # pylint: disable=no-member
             user, _ = User.objects.get_or_create(username=username)
 
             try:
-                key = args[1]
+                key = options['api_key']
                 set_user_auth_token(user, key)
             except AttributeError:
                 print "The key %s is in use by another user. Please select another key." % key

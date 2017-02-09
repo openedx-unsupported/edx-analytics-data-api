9361f2291f591f12c779dd726462f6747e9f12d3
Tyler Hallada <tyler@hallada.net>
2017-02-09 15:45:22 -0500

    AN-8236 Collapse "Upcoming" and "Starting soon" availability (#157)
    * Replace "Starting Soon" -> "Upcoming" availability
    
    "Starting Soon" is still stored in the database, but it is collapsed into
    the "Upcoming" category in the API view response.
    
    * Test that Starting Soon is unioned with Upcoming

--- analytics_data_api/management/commands/generate_fake_course_data.py
+++ analytics_data_api/management/commands/generate_fake_course_data.py
@@ -167,7 +167,7 @@ class Command(BaseCommand):
                 course_id=course_id, catalog_course_title='Demo Course', catalog_course='Demo_Course',
                 start_time=timezone.now() - datetime.timedelta(weeks=6),
                 end_time=timezone.now() + datetime.timedelta(weeks=10),
-                pacing_type='self_paced', availability='Current', enrollment_mode=mode, count=count,
+                pacing_type='self_paced', availability='Starting Soon', enrollment_mode=mode, count=count,
                 cumulative_count=cumulative_count, count_change_7_days=random.randint(-50, 50))
 
         progress.update(1)

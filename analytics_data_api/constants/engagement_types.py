775bb8bbdee63e245b0ebc3be1c117be55459603
Luis Moreno <luis.moreno@edunext.co>
2020-03-02 18:57:31 -0400

    Modernize analytics_data_api/constants/

--- analytics_data_api/constants/engagement_types.py
+++ analytics_data_api/constants/engagement_types.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from analytics_data_api.constants.engagement_events import (ATTEMPTED, ATTEMPTS_PER_COMPLETED, COMPLETED,
                                                             CONTRIBUTED, DISCUSSION, PROBLEM, VIDEO, VIEWED)
 

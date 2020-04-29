b84631972dfd4e08cdf8a2c439c139261e552482
Stuart Young <syoung@edx.org>
2020-04-29 17:05:00 -0400

    use index api instead of create

--- analytics_data_api/v0/tests/views/test_learners.py
+++ analytics_data_api/v0/tests/views/test_learners.py
@@ -97,7 +97,7 @@ class LearnerAPITestMixin(CsvViewMixin):
             if optional_field[1]:
                 body[optional_field[0]] = optional_field[1]
 
-        self._es.create(
+        self._es.index(
             index=settings.ELASTICSEARCH_LEARNERS_INDEX,
             doc_type='roster_entry',
             body=body
@@ -119,7 +119,7 @@ class LearnerAPITestMixin(CsvViewMixin):
         """
         Created an index with the date of when the learner index was updated.
         """
-        self._es.create(
+        self._es.index(
             index=settings.ELASTICSEARCH_LEARNERS_UPDATE_INDEX,
             doc_type='marker',
             body={

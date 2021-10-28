fd3eafa5e3e846b70c01bdc40e68f2490c793721
Usama Sadiq <usama.sadiq@arbisoft.com>
2021-10-28 12:49:36 +0500

    build: Replace Travis with GitHub CI

--- .travis/run_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-. /edx/app/analytics_api/venvs/analytics_api/bin/activate
-
-cd /edx/app/analytics_api/analytics_api
-
-make diff.report
-coverage xml
-bash ./scripts/build-stats-to-datadog.sh

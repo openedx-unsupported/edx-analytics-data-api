77c9a9a86cea1b25717347a820397b3f2cdca865
Zachary Hancock <zhancock@edx.org>
2022-04-21 16:21:03 -0400

    feat: format answer distribution fields (#547)
    * feat: format answer distribution fields
    
    * style: comment nit
    
    * fix: blank string for bad input
    
    Co-authored-by: edX requirements bot <devops+edx-requirements-bot@edx.org>

--- analytics_data_api/v0/tests/test_serializers.py
+++ analytics_data_api/v0/tests/test_serializers.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+import ddt
 from django.test import TestCase
 from django_dynamic_fixture import G
 
@@ -29,3 +30,47 @@ class DynamicFieldsModelSerializerTests(TestCase):
         instance = G(api_models.CourseEnrollmentDaily, course_id='3', count=1, date=now)
         serialized = TestSerializer(instance, exclude=('course_id',))
         self.assertListEqual(list(serialized.data.keys()), ['date', 'count', 'created'])
+
+
+@set_databases
+@ddt.ddt
+class ProblemResponseAnswerDistributionSerializerTests(TestCase):
+
+    @ddt.data(
+        ('', ''),
+        ('justastring', 'justastring'),
+        ('25.94', '25.94'),
+        ('[0, 100, 7, "x", "Duōshǎo"]', '[0|100|7|x|Duōshǎo]'),
+        ('"a","b" <choicehint>Correct.</choicehint> o', '"a","b" Correct. o'),
+        ('[{"3":"t1"},{"2":"t2"}]', '[{"3": "t1"}|{"2": "t2"}]'),
+        ('[[1,0,0],[0,1,0],[0,0,1],[0,0,0]]', '[[1, 0, 0]|[0, 1, 0]|[0, 0, 1]|[0, 0, 0]]'),
+        ('["<text>(S \\to D \\to G)</text>"]', '[(S \\to D \\to G)]'),
+        ('\\((2,1,3)^\\n\\)', '\\((2,1,3)^\\n\\)'),
+        (
+            'MLE [mathjax]\\widehat{\\theta }_ n^{\\text {MLE}}[/mathjax]',
+            'MLE [mathjax]\\widehat{\\theta }_ n^{\\text {MLE}}[/mathjax]'
+        ),
+        ('<img src="doggo.png" height="100"/>', ''),
+        ('<table><tr><td>(0</td><td>1/3</td><td>0)</td></tr></table>', '(0 1/3 0)'),
+    )
+    @ddt.unpack
+    def test_answer_formatting(self, answer_value, expected_display):
+        instance = G(
+            api_models.ProblemFirstLastResponseAnswerDistribution,
+            answer_value=answer_value,
+            value_id=answer_value
+        )
+        serialized = api_serializers.ProblemFirstLastResponseAnswerDistributionSerializer(instance)
+        self.assertEqual(serialized.data['answer_value'], expected_display)
+        self.assertEqual(serialized.data['value_id'], expected_display)
+
+    @ddt.data(
+        ('"hello**ONEBACKSLASHQUOTE**', '"hello\\"'),
+        ('**BACKSLASHSPACE**hello', '\\ hello'),
+        ('**FOURBACKSLASHQUOTE****TWOBACKSLASHQUOTE**', '\\\\\\\\"\\\\"'),
+    )
+    @ddt.unpack
+    def test_answer_value_unslugged(self, answer_value, expected_display):
+        instance = G(api_models.ProblemFirstLastResponseAnswerDistribution, answer_value=answer_value)
+        serialized = api_serializers.ProblemFirstLastResponseAnswerDistributionSerializer(instance)
+        self.assertEqual(serialized.data['answer_value'], expected_display)
